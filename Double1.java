d17a9a1d325c896cfd200fba5a904b055dd1a54b
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-04-22 19:14:28 +0400

    Update Double1.java

--- Double1.java
+++ Double1.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class Double1 {
- public static void main(String[] arg) throws Exception{
-   Scanner s = new Scanner(System.in);
-   double a = s.nextDouble();
+  public static void main(String[] arg) throws Exception {
+    Scanner s = new Scanner(System.in);
+    double a = s.nextDouble();
 
-  if ((a % 1) == 0) {
-   System.out.println("It's not a double number");
-  } else {
-   throw new Exception ("It's a double number");
+    if ((a % 1) == 0) {
+      System.out.println("It's not a double number");
+    } else {
+      throw new Exception("It's a double number");
+    }
   }
- }
 }
