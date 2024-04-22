c8901482080adbc96de4f726fe793dc45464a3c7
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-04-22 19:13:06 +0400

    Add files via upload

---
+++ Double1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Double1 {
+ public static void main(String[] arg) throws Exception{
+   Scanner s = new Scanner(System.in);
+   double a = s.nextDouble();
+
+  if ((a % 1) == 0) {
+   System.out.println("It's not a double number");
+  } else {
+   throw new Exception ("It's a double number");
+  }
+ }
+}
